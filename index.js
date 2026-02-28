80a6610bdadbc6ab8d91eec4bbfb6bf6bc785023
Palak aggarwal <palakaggarwalaol@gmail.com>
2026-02-28 20:58:29 +0530

    Signin and Signup pages created

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 8000;
+
+const userRoute = require('./routes/user');
+
+mongoose.connect('mongodb://localhost:27017/node_blogify').then(e=> console.log("MongoDB Connected..."));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine','ejs');
+app.set('views',path.resolve('./views'));
+
+app.get("/",(req,res)=>{
+    res.render('home');
+});
+
+app.use('/user',userRoute);
+
+app.listen(PORT,()=>console.log(`Server Started at PORT: ${PORT}`));
